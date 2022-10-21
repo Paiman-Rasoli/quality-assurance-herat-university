7a5777e38da74e9b94d1dc464060674e78fc3f6c
Paiman-Rasoli <paimanrasoli789@gmail.com>
2022-10-21 19:04:26 +0430

    teacher routes finalized

--- qa-backend/src/routes/teacher.routes.ts
+++ qa-backend/src/routes/teacher.routes.ts
@@ -1,14 +1,42 @@
 import { Router } from "express";
 import { TeacherService } from "../services";
 import { authGuard } from "../middlewares/passport";
-import { check } from "express-validator";
+import { body, check, query } from "express-validator";
 
 const teacherService = new TeacherService();
 
-export const routes = Router();
+const routes = Router();
+routes.get("/", authGuard, teacherService.all);
 routes.post(
-  "/add",
-  [check("username").not().isEmpty(), check("password").not().isEmpty()],
+  "/",
+  [
+    check("fa_name").isString().notEmpty(),
+    check("en_name").isString().notEmpty(),
+    check("date").isDate(),
+    check("departmentId").notEmpty(),
+  ],
   authGuard,
-  teacherService.addTeacher
+  teacherService.create
 );
+
+routes.put(
+  "/",
+  [body("id").notEmpty().withMessage("id is required!")],
+  authGuard,
+  teacherService.update
+);
+
+routes.get(
+  "/find-one",
+  [query("id").notEmpty().withMessage("id is required!")],
+  authGuard,
+  teacherService.findOne
+);
+routes.delete(
+  "/",
+  [body("id").notEmpty().withMessage("id is required!")],
+  authGuard,
+  teacherService.delete
+);
+
+export { routes };
