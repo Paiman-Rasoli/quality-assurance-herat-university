75f08e4535e2accdc327f410d1e91435782869aa
Paiman-Rasoli <paimanrasoli789@gmail.com>
2022-10-21 12:36:44 +0430

    Finalizing department routes

--- qa-backend/src/services/department.service.ts
+++ qa-backend/src/services/department.service.ts
@@ -5,7 +5,7 @@ import { DepartmentEntity } from "../entities";
 import { logger } from "../lib";
 
 export class DepartmentService {
-  async find(req: Request, res: Response) {
+  async all(req: Request, res: Response) {
     const departmentModel = getMyRepository(DepartmentEntity);
     let data;
     // check if request wants paginated data
@@ -56,5 +56,56 @@ export class DepartmentService {
         .status(500)
         .json({ msg: "Server Error occurred see your log." });
     }
+  }
+
+  async update(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const departmentModel = getMyRepository(DepartmentEntity);
+    const findOne = await departmentModel.update(
+      {
+        id: +req.body.id,
+      },
+      {
+        ...req.body,
+        createdAt: new Date(),
+      }
+    );
+
+    return res
+      .status(findOne.affected > 0 ? 200 : 500)
+      .json({ updated: findOne.affected > 0 });
+  }
+
+  async delete(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const departmentModel = getMyRepository(DepartmentEntity);
+    const deleteOne = await departmentModel.delete({
+      id: +req.body.id,
+    });
+    return res
+      .status(deleteOne.affected > 0 ? 200 : 400)
+      .json({ deleted: deleteOne.affected > 0 });
+  }
+
+  async findOne(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const departmentModel = getMyRepository(DepartmentEntity);
+    const find = await departmentModel.findOne({
+      where: {
+        id: +req.query.id,
+      },
+      relations: ["faculty"],
+    });
+
+    return res.status(200).json(find);
   }
 }
