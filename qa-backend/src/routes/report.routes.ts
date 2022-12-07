77f97a198554bd6f68debf9cb7d2afaca7eb194a
Mustafa Zahedi <96369008+Mustafa-Zahedi@users.noreply.github.com>
2022-12-07 16:35:02 +0430

    feat: reports for dep and teacher on backend and chart js added on front

--- qa-backend/src/routes/report.routes.ts
+++ qa-backend/src/routes/report.routes.ts
@@ -7,14 +7,28 @@ const reportService = new ReportService();
 
 const routes = Router();
 routes.post(
-  "/",
+  "/department",
   [
     body("departmentId").notEmpty(),
+    body("semester").notEmpty(),
     body("year").notEmpty(),
     body("type").notEmpty(),
   ],
   authGuard,
-  reportService.report
+  reportService.departmentReport
+);
+
+routes.post(
+  "/teacher",
+  [
+    body("departmentId").notEmpty(),
+    body("teacherId").notEmpty(),
+    body("semester").notEmpty(),
+    body("year").notEmpty(),
+    body("type").notEmpty(),
+  ],
+  authGuard,
+  reportService.teacherReport
 );
 
 routes.post(
---
+++ qa-frontend/src/components/reports/request.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const RequestReport = ({ depReport }) => {
+  console.log("depReport", depReport);
+  return <section>{}</section>;
+};
+
+export default RequestReport;
