c79fc490ec3dc4357605bf38b3c2ec4239d879dc
Mustafa Zahedi <96369008+Mustafa-Zahedi@users.noreply.github.com>
2022-12-12 11:52:05 +0430

    feat: some new report services and inputYear added

--- qa-frontend/src/components/reports/depReport.jsx
+++ qa-frontend/src/components/reports/depReport.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import Loading from "../loading";
 import { httpGetReport } from "../../services/report";
 import { BarChart } from "./barChart";
+import { toast } from "react-toastify";
+import { ToastMsg } from "../TaostMsg";
 
 const DepartmentReport = () => {
+  const [loading, setLoading] = useState(false);
   const [depReport, setDepReport] = useState([]);
   const [chartData, setChartData] = useState([]);
   const [response, setResponse] = useState(null);
@@ -12,29 +15,40 @@ const DepartmentReport = () => {
 
   useEffect(() => {
     (async function () {
-      const res = await httpGetReport(
-        {
-          departmentId: 1,
-          year: 2022,
-          semester_type: "بهاری",
-        },
-        "department"
-      );
-      setResponse(res);
-      const reports = await res.json();
-      setDepReport(reports);
-      setChartData(
-        reports?.teachersRep?.map((item) => ({
-          percent: item?.percent,
-          label: item?.teacherId,
-        }))
-      );
-      console.log("dep-report", reports, depReport);
+      setLoading(true);
+      try {
+        const res = await httpGetReport(
+          {
+            departmentId: 2,
+            year: 2022,
+            semester_type: "بهاری",
+          },
+          "department"
+        );
+        setResponse(res);
+        const reports = await res.json();
+        setDepReport(reports);
+        setChartData(
+          reports?.teachersRep?.map((item) => ({
+            percent: item?.percent,
+            label: item?.teacherId,
+          }))
+        );
+        console.log("dep-report", reports, depReport);
+      } catch (error) {
+        toast.warning(<ToastMsg text={"خطا در بارگیری دیتا"} />);
+      } finally {
+        setLoading(false);
+      }
     })();
   }, []);
 
-  if (depReport.length < 1) return <Loading />;
-  if (response.status === 404) return <section>اطلاعاتی یافت نشد</section>;
+  if (loading)
+    <div>
+      <Loading />
+    </div>;
+
+  if (response?.status === 404) return <section>اطلاعاتی یافت نشد</section>;
 
   return (
     <section className="font-vazirBold p-2 md:p-5 lg:p-10 w-full">
@@ -56,27 +70,36 @@ const DepartmentReport = () => {
           <span>{depReport?.semester_type}</span>
         </li>
       </ul>
-      <article className="flex gap-2 flex-wrap justify-around m-5">
-        <div className="flex gap-3 bg-orange-300 rounded p-3">
-          <span>فیصدی امتیازات دیپارتمنت</span>
-          <span>
-            {Number(depReport?.total?.percent).toFixed(1).toString()}%
-          </span>
-        </div>
-        <div className="flex gap-3 bg-orange-300 rounded p-3">
-          <span>تعداد اساتید شامل این گزارش</span>
-          <span>{depReport?.teachersRep?.length}</span>
-        </div>
-        <div className="flex gap-3 bg-orange-300 rounded p-3">
-          <span>تعداد مضامین شامل این گزارش</span>
-          <span>{depReport?.totalSubject}</span>
-        </div>
-        <div className="flex gap-3 bg-orange-300 rounded p-3">
-          <span>تعداد اشتراک کننده</span>
-          <span>{Number(depReport?.total?.subscribers)}</span>
-        </div>
-      </article>
-      <div>{chartData?.length > 0 && <BarChart chartData={chartData} />}</div>
+
+      {depReport?.total?.subscribers === 0 ? (
+        <div>هنوز هیچ کسی اشتراک نکرده</div>
+      ) : (
+        <>
+          <article className="flex gap-2 flex-wrap justify-around m-5">
+            <div className="flex gap-3 bg-orange-300 rounded p-3">
+              <span>فیصدی امتیازات دیپارتمنت</span>
+              <span>
+                {Number(depReport?.total?.percent).toFixed(1).toString()}%
+              </span>
+            </div>
+            <div className="flex gap-3 bg-orange-300 rounded p-3">
+              <span>تعداد اساتید شامل این گزارش</span>
+              <span>{depReport?.teachersRep?.length}</span>
+            </div>
+            <div className="flex gap-3 bg-orange-300 rounded p-3">
+              <span>تعداد مضامین شامل این گزارش</span>
+              <span>{depReport?.totalSubject}</span>
+            </div>
+            <div className="flex gap-3 bg-orange-300 rounded p-3">
+              <span>تعداد اشتراک کننده</span>
+              <span>{Number(depReport?.total?.subscribers)}</span>
+            </div>
+          </article>
+          <div>
+            {chartData?.length > 0 && <BarChart chartData={chartData} />}
+          </div>
+        </>
+      )}
     </section>
   );
 };
